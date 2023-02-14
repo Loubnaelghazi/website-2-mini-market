d646807edcf6abc9428f704abd74d4cc8f0c690c
Loubnaelghazi <114692135+Loubnaelghazi@users.noreply.github.com>
2023-02-14 21:47:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+//categories
+
+//les fruits et legumes
+document.getElementById('fruits').addEventListener("click", function () {
+  document.getElementById('reco').style.display = "none";
+  document.getElementById('fakiha').style.display = "block";
+  document.getElementById('7anout').style.display = "none";
+  document.getElementById('l7em').style.display = "none";
+  document.getElementById('chrib').style.display = "none";
+
+
+});
+//bouton decouvrir fruits et legumes
+document.getElementById('fawakih').addEventListener("click", function () {
+  document.getElementById('reco').style.display = "none";
+  document.getElementById('fakiha').style.display = "block";
+  document.getElementById('7anout').style.display = "none";
+  document.getElementById('l7em').style.display = "none";
+  document.getElementById('chrib').style.display = "none";
+});
+
+
+//les viandes
+document.getElementById('viandes').addEventListener("click", function () {
+  document.getElementById('reco').style.display = "none";
+  document.getElementById('l7em').style.display = "block";
+  document.getElementById('7anout').style.display = "none";
+  document.getElementById('fakiha').style.display = "none";
+  document.getElementById('chrib').style.display = "none";
+
+});
+
+//bouton decouvrir viandes
+document.getElementById('meat').addEventListener("click", function () {
+  document.getElementById('reco').style.display = "none";
+  document.getElementById('l7em').style.display = "block";
+  document.getElementById('7anout').style.display = "none";
+  document.getElementById('fakiha').style.display = "none";
+  document.getElementById('chrib').style.display = "none";
+});
+
+//l epicerie
+document.getElementById('epicerie').addEventListener("click", function () {
+  document.getElementById('reco').style.display = "none";
+  document.getElementById('7anout').style.display = "block";
+  document.getElementById('fakiha').style.display = "none";
+  document.getElementById('l7em').style.display = "none";
+  document.getElementById('chrib').style.display = "none";
+
+});
+//bouton decouvrir epicerie
+document.getElementById('epic').addEventListener("click", function () {
+  document.getElementById('reco').style.display = "none";
+  document.getElementById('7anout').style.display = "block";
+  document.getElementById('fakiha').style.display = "none";
+  document.getElementById('l7em').style.display = "none";
+  document.getElementById('chrib').style.display = "none";
+
+
+});
+
+//les boissons
+document.getElementById('eaux').addEventListener("click", function () {
+  document.getElementById('chrib').style.display = "block";
+  document.getElementById('fakiha').style.display = "none";
+  document.getElementById('7anout').style.display = "none";
+  document.getElementById('reco').style.display = "none";
+  document.getElementById('l7em').style.display = "none";
+});
+
+//bouton decouvrir boissons
+document.getElementById('water').addEventListener("click", function () {
+  document.getElementById('chrib').style.display = "block";
+  document.getElementById('fakiha').style.display = "none";
+  document.getElementById('7anout').style.display = "none";
+  document.getElementById('reco').style.display = "none";
+  document.getElementById('l7em').style.display = "none";
+});
+
+/////////////////////////////////////////////////////////////////////////////////
---
+++ script2.js
@@ -0,0 +1,81 @@
+//categories
+//js dyl page dyl administration
+//les fruits et legumes
+document.getElementById('fraise').addEventListener("click", function () {
+    
+    document.getElementById('fruta').style.display = "block";
+    document.getElementById('lubnazin').style.display = "none";
+    document.getElementById('la7m').style.display = "none";
+    document.getElementById('chrab').style.display = "none";
+  
+  
+  });
+  //bouton decouvrir fruits et legumes
+  document.getElementById('lubna').addEventListener("click", function () {
+   
+    document.getElementById('fruta').style.display = "block";
+    document.getElementById('').style.display = "none";
+    document.getElementById('').style.display = "none";
+    document.getElementById('').style.display = "none";
+  });
+  
+  
+  //les viandes
+  document.getElementById('dl3a').addEventListener("click", function () {
+   
+    document.getElementById('la7m').style.display = "block";
+    document.getElementById('lubnazin').style.display = "none";
+    document.getElementById('fruta').style.display = "none";
+    document.getElementById('chrab').style.display = "none";
+  
+  });
+  
+  //bouton decouvrir viandes
+  document.getElementById('fagussa').addEventListener("click", function () {
+   
+    document.getElementById('la7m').style.display = "block";
+    document.getElementById('lubnazin').style.display = "none";
+    document.getElementById('fruta').style.display = "none";
+    document.getElementById('chrab').style.display = "none";
+  });
+  
+  //l epicerie
+  document.getElementById('9adour').addEventListener("click", function () {
+   
+    document.getElementById('lubnazin').style.display = "block";
+    document.getElementById('fruta').style.display = "none";
+    document.getElementById('la7m').style.display = "none";
+    document.getElementById('chrab').style.display = "none";
+  
+  });
+  //bouton decouvrir epicerie
+  document.getElementById('mustapha').addEventListener("click", function () {
+  
+    document.getElementById('lubnazin').style.display = "block";
+    document.getElementById('fruta').style.display = "none";
+    document.getElementById('la7m').style.display = "none";
+    document.getElementById('chrab').style.display = "none";
+  
+  
+  });
+  
+  //les boissons
+  document.getElementById('hanan').addEventListener("click", function () {
+    document.getElementById('chrab').style.display = "block";
+    document.getElementById('fruta').style.display = "none";
+    document.getElementById('lubnazin').style.display = "none";
+  
+    document.getElementById('la7m').style.display = "none";
+  });
+  
+  //bouton decouvrir boissons
+  document.getElementById('fatimamsmuma').addEventListener("click", function () {
+    document.getElementById('chrab').style.display = "block";
+    document.getElementById('fruta').style.display = "none";
+    document.getElementById('lubnazin').style.display = "none";
+  
+    document.getElementById('la7m').style.display = "none";
+  });
+  
+  /////////////////////////////////////////////////////////////////////////////////
+  
